5bf7edc0a472345af8c9ad33ae8bc0eeeff3c0d9
gc6123@bard.edu <gc6123@bard.edu>
2018-08-16 06:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays(string) {return "Happy holidays!"}
 function happyHolidaysTo(Max) {return `Happy holidays, ${Max}!`}
 function happyHolidayTo(Christmas, Charles) {
   return `Happy ${Christmas}, ${Charles}!`}
-function holidayCountdown(birthday, '7') {
+function holidayCountdown(birthday, "7") {
   return `It's ${'7'} days until ${birthday}!`}
